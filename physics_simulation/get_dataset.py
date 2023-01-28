c6cfbf10c41e46f6c8279960f04b6c82c8f2f2f4
p.menegatti <p.menegatti@omia.fr>
2023-01-29 00:01:07 +0100

    rewrite path

--- physics_simulation/get_dataset.py
+++ physics_simulation/get_dataset.py
@@ -25,8 +25,8 @@ conf.set_derivate_parameters()
 def get_dataset(DATASET_NAME="WaterDrop"):
     BASE_URL=f"https://storage.googleapis.com/learning-to-simulate-complex-physics/Datasets/{DATASET_NAME}/"
 
-    water_drop_dir = conf.ROOT_DATASET / 'water_drop'
-    os.makedirs(str(water_drop_dir), exist_ok=True)
+    dataset_dir = conf.ROOT_DATASET / DATASET_NAME
+    os.makedirs(str(dataset_dir), exist_ok=True)
 
     for file in ["metadata.json", "train.tfrecord", "valid.tfrecord", "test.tfrecord"]:
         wget.download(f"{BASE_URL}{file}", "dataset/" + DATASET_NAME)
